13e02206d43195dbe58cff81e7964f83a76ef442
yhayashi30 <yoshiyuki.hayashi@outlook.com>
2020-05-06 05:59:32 +0000

    update abc-163-d

--- 20200417-ABC/D.py
+++ 20200417-ABC/D.py
@@ -1,23 +1,14 @@
 N, K = map(int, input().split())
 
-def cmb(n, r, mod):
-	if ( r<0 or r>n ):
-		return 0
-	r = min(r, n-r)
-	return g1[n] * g2[r] * g2[n-r] % mod
+def sumLR(l, r):
+	return (l+r)*(r-l+1)//2
 
-mod = 10**9+7 #出力の制限
-M = N+1
-g1 = [1, 1] # 元テーブル
-g2 = [1, 1] #逆元テーブル
-inverse = [0, 1] #逆元テーブル計算用テーブル
+mod = 10**9 + 7
+count = 0
 
-for i in range( 2, M + 1 ):
-	g1.append( ( g1[-1] * i ) % mod )
-	inverse.append( ( -inverse[mod % i] * (mod//i) ) % mod )
-	g2.append( (g2[-1] * inverse[-1]) % mod )
-    
-ans = 0
-for j in range(K, N+1):
-	ans += cmb(N+1, j, mod)
-print(ans)
+for n in range(K, N+2):
+	max = sumLR(N-n+1, N)
+	min = sumLR(0, n-1)
+	count += max - min + 1
+	count %= mod
+print(count)
